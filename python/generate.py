ea115d250d0701a6ad98581d7eb9ee0b6e2cc0bf
Mambix <ledi.mambix@gmail.com>
2017-02-25 15:43:49 +0000

    first draft of script for generating cut files

--- python/generate.py
+++ python/generate.py
@@ -1,18 +1,23 @@
 import ezdxf
 import os
+import operator
+
 
 def addLayer(layerName, dxf, srcDxf):
     if layerName not in dxf.layers:
         lay = srcDxf.layers.get(layerName)
-        dxf.layers.new(name=lay.dxf.name, dxfattribs={'linetype': lay.dxf.linetype, 'color': lay.dxf.color, 'line_weight': lay.dxf.line_weight})
+        dxf.layers.new(name=lay.dxf.name, dxfattribs={'linetype': lay.dxf.linetype, 'color': lay.dxf.color})
 
-def mergeDXF(target, source, move = None):
+def mergeDXF(target, source, move = (0.0, 0.0)):
     if not os.path.isfile(source):
         return False
     if not ezdxf.is_dxf_file(source):
         return False
     src = ezdxf.readfile(source)
+    print('Adding {}[{}] to {}'.format(source, src.dxfversion, target))
     dxf = ezdxf.new(src.dxfversion)
+    lay = dxf.layers.get('0')
+    lay.set_color(2)
     if os.path.isfile(target):
         if ezdxf.is_dxf_file(target):
             dxf = ezdxf.readfile(target)
@@ -22,16 +27,23 @@ def mergeDXF(target, source, move = None):
     # for lay in src.layers:
     for e in source_msp:
         t = '{}'.format(e.dxftype)
-        if t[14:20] == 'Circle':
+        if t[14:17] == 'Arc':
             addLayer(e.dxf.layer, dxf, src)
-            target_msp.add_circle(e.dxf.center, e.dxf.radius, dxfattribs={'layer': e.dxf.layer})
+            c = tuple(map(operator.add, e.dxf.center, move))
+            target_msp.add_arc(c, e.dxf.radius, e.dxf.start_angle, e.dxf.end_angle, dxfattribs={'layer': e.dxf.layer})
+        elif t[14:20] == 'Circle':
+            addLayer(e.dxf.layer, dxf, src)
+            c = tuple(map(operator.add, e.dxf.center, move))
+            target_msp.add_circle(c, e.dxf.radius, dxfattribs={'layer': e.dxf.layer})
         elif t[14:19] == 'Point':
             addLayer(e.dxf.layer, dxf, src)
-            target_msp.add_point(e.dxf.location, dxfattribs={'layer': e.dxf.layer})
+            loc = tuple(map(operator.add, e.dxf.location, move))
+            target_msp.add_point(loc, dxfattribs={'layer': e.dxf.layer})
         elif t[14:18] == 'Text':
             addLayer(e.dxf.layer, dxf, src)
+            c = (e.dxf.insert[0]+move[0],e.dxf.insert[1]+move[1], e.dxf.insert[2])
             target_msp.add_text(e.dxf.text, dxfattribs={
-                'insert': e.dxf.insert,
+                'insert': c,
                 'height': e.dxf.height,
                 'rotation': e.dxf.rotation,
                 'style': e.dxf.style,
@@ -41,9 +53,11 @@ def mergeDXF(target, source, move = None):
                 'layer': e.dxf.layer
             })
         elif t[14:19] == 'MText':
+            # print('MTEXT: {}'.format(e.get_text()))
             addLayer(e.dxf.layer, dxf, src)
+            c = (e.dxf.insert[0] + move[0], e.dxf.insert[1] + move[1], e.dxf.insert[2])
             target_msp.add_mtext(e.get_text(), dxfattribs={
-                'insert': e.dxf.insert,
+                'insert': c,
                 'char_height': e.dxf.char_height,
                 'width': e.dxf.width,
                 'attachment_point': e.dxf.attachment_point,
@@ -56,34 +70,74 @@ def mergeDXF(target, source, move = None):
             })
         elif t[14:18] == 'Line':
             addLayer(e.dxf.layer, dxf, src)
-            target_msp.add_line(e.dxf.start, e.dxf.end, dxfattribs={'layer': e.dxf.layer})
-        elif t[14:20] == 'LWPoly':
-            print e.dxf.flags
+            start = tuple(map(operator.add, e.dxf.start, move))
+            end = tuple(map(operator.add, e.dxf.end, move))
+            target_msp.add_line(start, end, dxfattribs={'layer': e.dxf.layer})
+        elif t[14:22] == 'Polyline':
             addLayer(e.dxf.layer, dxf, src)
-            target_msp.add_lwpolyline(e.get_points(), dxfattribs={
+            points = e.points()
+            newPoints = []
+            for p in points:
+                newPoints.append((p[0] + move[0], p[1] + move[1]))
+            # poly = target_msp.add_polyline(newPoints, dxfattribs={
+            #     'layer': e.dxf.layer,
+            #     'flags': e.dxf.flags
+            # })
+            # poly.closed = e.closed
+        elif t[14:20] == 'LWPoly':
+            addLayer(e.dxf.layer, dxf, src)
+            points = e.get_rstrip_points()
+            newPoints = []
+            for p in points:
+                newPoints.append( (p[0] + move[0], p[1] + move[1]) )
+            poly = target_msp.add_lwpolyline(newPoints, dxfattribs={
                 'layer': e.dxf.layer,
                 'flags': e.dxf.flags
             })
-        # elif t[14:20] == 'Modern':
-        #     addLayer(e.dxf.layer, dxf, src)
-        #     target_msp.add_entity(e)
+            poly.closed = e.closed
+        elif t[14:20] == 'Spline':
+            addLayer(e.dxf.layer, dxf, src)
+            points = e.get_fit_points()
+            newPoints = []
+            for p in points:
+                newPoints.append( (p[0] + move[0], p[1] + move[1], p[2]) )
+            spline = target_msp.add_spline(newPoints, dxfattribs={
+                'layer': e.dxf.layer,
+                'flags': e.dxf.flags
+            })
+            spline.closed = e.closed
+        elif t[14:20] == 'Modern':
+            # addLayer(e.dxf.layer, dxf, src)
+            # target_msp.add_entity(e)
+            ignore = 1
         else:
             print t[14:]
             print e.dxf.layer
+            # target_msp.add_entity(e)
         # source_msp.unlink_entity(e)
         # target_msp.add_entity(e)
 
     dxf.saveas(target)
     return True
 
-if __name__ == "__main__":
-    if os.path.isfile('../dxf/6mm_plywood_cut_left_right.dxf'):
-        os.remove('../dxf/6mm_plywood_cut_left_right.dxf')
-    mergeDXF('../dxf/6mm_plywood_cut_left_right.dxf', '../dxf/blocks/6mm_les_izrez_levo - LM8UU.dxf')
-    mergeDXF('../dxf/6mm_plywood_cut_left_right.dxf', '../dxf/blocks/6mm_les_izrez_desno - LM8UU.dxf')
+def mergeFiles(target, sources):
+    if os.path.isfile(target):
+        os.remove(target)
+    for source, move in sources.iteritems():
+        mergeDXF(target, source, move)
 
-# dwg = '../dxf/blocks/6mm_les_izrez_dol - LM8UU.dxf'
-# msp = dwg.modelspace()
-#
-# dxf_cuts = msp.query('*[layer=="IZREZ"]')
-# dxf_holes = msp.query('*[layer=="HOLES"]')
+if __name__ == "__main__":
+    mergeFiles('../dxf/6mm_plywood_cut_left_right.dxf', {
+        '../dxf/blocks/6mm_les_izrez_levo - LM8UU.dxf': (0.0, 0.0),
+        '../dxf/blocks/6mm_les_izrez_desno - LM8UU.dxf': (0.0, 0.0)
+    })
+
+    mergeFiles('../dxf/6mm_plywood_cut_front_back.dxf', {
+        '../dxf/blocks/6mm_les_izrez_spredaj - LM8UU.dxf': (0.0, 0.0),
+        '../dxf/blocks/6mm_les_izrez_zadaj - LM8UU.dxf': (0.0, 0.0)
+    })
+
+    mergeFiles('../dxf/6mm_plywood_cut_top_bottom.dxf', {
+        '../dxf/blocks/6mm_les_izrez_gor - LM8UU.dxf': (-206.0, 205.0),
+        '../dxf/blocks/6mm_les_izrez_dol - LM8UU.dxf': (206.0, 205.0)
+    })
